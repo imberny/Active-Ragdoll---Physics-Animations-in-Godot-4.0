7b87834e3b280c482da00af89c55401fcf702d0b
imberny <bernard.p.cloutier@gmail.com>
2024-10-02 21:18:48 -0400

    Fix skeleton rotation

--- scenes/interpolated_skeleton_modifier_3d.gd
+++ scenes/interpolated_skeleton_modifier_3d.gd
@@ -25,15 +25,12 @@ func _on_physics_skeleton_updated():
 				i,
 				(
 					global_transform.affine_inverse()
-					* (
-						animated_transform
-						. interpolate_with(
-							# don't ask me why I'm rotating by -90 degrees, I don't know either. Something about
-							# how the models were setup I guess.
-							physics_transform.rotated(Vector3.MODEL_TOP, -PI / 2.0),
-							physics_interpolation
-						)
-					)
+					# don't ask me why I'm rotating by -90 degrees, I don't know either. Something about
+					# how the models were setup I guess.
+					* (animated_transform.rotated(Vector3.MODEL_TOP, -PI / 2.0).interpolate_with(
+						physics_transform.rotated(Vector3.MODEL_TOP, -PI / 2.0),
+						physics_interpolation
+					))
 				)
 			)
 		)
